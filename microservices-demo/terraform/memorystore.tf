43ef2122a1c7088c21b18c057366ce55791e3c8e
Shorya Verma <shoryaverma54@gmail.com>
2026-08-25 23:34:07 +0530

    Fix accidental paste into memorystore.tf

--- microservices-demo/terraform/memorystore.tf
+++ microservices-demo/terraform/memorystore.tf
@@ -31,8 +31,7 @@ resource "google_redis_instance" "redis-cart" {
 }
 
 # Edit contents of Memorystore kustomization.yaml file to target new Memorystore (redis) instance
-resource "null_resource" "kustomization-update" {git commit -m "Initial commit: Online Boutique running locally on kind (Week 1)"
-
+resource "null_resource" "kustomization-update" {
   provisioner "local-exec" {
     interpreter = ["bash", "-exc"]
     command     = "sed -i \"s/REDIS_CONNECTION_STRING/${google_redis_instance.redis-cart[0].host}:${google_redis_instance.redis-cart[0].port}/g\" ../kustomize/components/memorystore/kustomization.yaml"
